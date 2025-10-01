8c3c1b7030f2d0b31fd913d8d993b7d8a0cb4544
jr-public <jorgeruiz.kj@gmail.com>
2025-10-01 19:09:49 +0200

    Improves handling of failed authentication attempts

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -2,8 +2,7 @@
 namespace App\Security;
 
 use App\Entity\User;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use App\Exception\AuthException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -12,23 +11,21 @@ class UserChecker implements UserCheckerInterface
     public function checkPreAuth(UserInterface $user): void
     {
         if (!$user instanceof User) {
-            throw new AccessDeniedException('Invalid user type');
-            return;
-        }
-
-        if (!$user->isActivated()) {
-            throw new CustomUserMessageAccountStatusException('Account pending activation');
-        }
-        if ($user->isSuspended()) {
-            throw new CustomUserMessageAccountStatusException('Account suspended');
+            throw new AuthException('AUTH_ERROR', 'BAD_USER_TYPE', 500);
         }
     }
 
     public function checkPostAuth(UserInterface $user): void
     {
         if (!$user instanceof User) {
-            throw new AccessDeniedException('Invalid user type');
-            return;
+            throw new AuthException('AUTH_ERROR', 'BAD_USER_TYPE', 500);
+        }
+
+        if (!$user->isActivated()) {
+            throw new AuthException('AUTH_ERROR', 'INACTIVE_USER', 401);
+        }
+        if ($user->isSuspended()) {
+            throw new AuthException('AUTH_ERROR', 'SUSPENDED_USER', 403);
         }
     }
 }
