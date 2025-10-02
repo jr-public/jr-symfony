1a3974b2c76dc3f2af4cc19a1fed42808679899c
jr-public <jorgeruiz.kj@gmail.com>
2025-10-02 16:33:15 +0200

    update: ApiException now stores a $thrownBy throwable
    Updated toArray()

--- src/Exception/ApiException.php
+++ src/Exception/ApiException.php
@@ -4,6 +4,8 @@ namespace App\Exception;
 class ApiException extends \Exception {
     protected ?string $detail;
     protected int $httpStatus;
+    protected ?\Throwable $thrownBy = null;
+
     public function __construct(
         string $message = 'API_ERROR',
         ?string $detail = null,
@@ -13,7 +15,27 @@ class ApiException extends \Exception {
         $this->httpStatus = $httpStatus;
         $this->detail = $detail;
     }
-
+    public function toArray(): array {
+        $array = [
+            'class'     => get_class($this),
+            'message'   => $this->getMessage(),
+            'detail'    => $this->getDetail(),
+            'status'    => $this->getHttpStatus(),
+            'file'      => $this->getFile(),
+            'line'      => $this->getLine(),
+        ];
+        $thrownBy = $this->getThrownBy();
+        if ($thrownBy) {
+            $array['thrownBy'] = [
+                'class'     => get_class($thrownBy),
+                'message'   => $thrownBy->getMessage(),
+                'status'      => $thrownBy->getCode(),
+                'file'      => $thrownBy->getFile(),
+                'line'      => $thrownBy->getLine(),
+            ];
+        }
+        return $array;
+    }
     public function getDetail(): ?string {
         if (empty($this->detail)) {
             return $this->message;
@@ -22,5 +44,11 @@ class ApiException extends \Exception {
     }
     public function getHttpStatus(): int {
         return $this->httpStatus;
+    }
+    public function setThrownBy(\Throwable $throwable): void {
+        $this->thrownBy = $throwable;
+    }
+    public function getThrownBy(): ?\Throwable {
+        return $this->thrownBy;
     }
 }
